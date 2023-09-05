a54e9183b1e9955bddb224e488a29cdaa7725b73
ryotarofr <ryoryo.fr0608@icloud.com>
2023-09-05 11:22:39 +0900

    test

---
+++ prisma/migrations/20230905022128_/migration.sql
@@ -0,0 +1,58 @@
+-- CreateEnum
+CREATE TYPE "EvaluationType" AS ENUM ('A', 'B', 'C', 'D', 'E');
+
+-- CreateTable
+CREATE TABLE "Naisei" (
+    "id" SERIAL NOT NULL,
+    "naisei" TEXT NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "evaluation_type" "EvaluationType" NOT NULL,
+    "userId" TEXT,
+
+    CONSTRAINT "Naisei_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Assessment" (
+    "id" TEXT NOT NULL,
+    "userId" TEXT NOT NULL,
+    "achievement" INTEGER,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "Assessment_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "UserApiLimit" (
+    "id" TEXT NOT NULL,
+    "userId" TEXT NOT NULL,
+    "count" INTEGER NOT NULL DEFAULT 0,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "UserApiLimit_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "UserSubscription" (
+    "id" TEXT NOT NULL,
+    "userId" TEXT NOT NULL,
+    "stripe_customer_id" TEXT,
+    "stripe_subscription_id" TEXT,
+    "stripe_price_id" TEXT,
+    "stripe_current_period_end" TIMESTAMP(3),
+
+    CONSTRAINT "UserSubscription_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "UserApiLimit_userId_key" ON "UserApiLimit"("userId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "UserSubscription_userId_key" ON "UserSubscription"("userId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "UserSubscription_stripe_customer_id_key" ON "UserSubscription"("stripe_customer_id");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "UserSubscription_stripe_subscription_id_key" ON "UserSubscription"("stripe_subscription_id");
