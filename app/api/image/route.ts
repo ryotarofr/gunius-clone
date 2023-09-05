65b7084556e5ffbc4a837d6cce7a39ece312cb34
ryotarofr <ryoryo.fr0608@icloud.com>
2023-09-05 10:16:54 +0900

    test

--- app/api/image/route.ts
+++ app/api/image/route.ts
@@ -5,11 +5,11 @@ import { Configuration, OpenAIApi } from "openai";
 import { checkSubscription } from "@/lib/subscription";
 import { incrementApiLimit, checkApiLimit } from "@/lib/api-limit";
 
-const configuration = new Configuration({
-  apiKey: process.env.OPENAI_API_KEY,
-});
+// const configuration = new Configuration({
+//   apiKey: process.env.OPENAI_API_KEY,
+// });
 
-const openai = new OpenAIApi(configuration);
+// const openai = new OpenAIApi(configuration);
 
 export async function POST(
   req: Request
@@ -23,9 +23,9 @@ export async function POST(
       return new NextResponse("Unauthorized", { status: 401 });
     }
 
-    if (!configuration.apiKey) {
-      return new NextResponse("OpenAI API Key not configured.", { status: 500 });
-    }
+    // if (!configuration.apiKey) {
+    //   return new NextResponse("OpenAI API Key not configured.", { status: 500 });
+    // }
 
     if (!prompt) {
       return new NextResponse("Prompt is required", { status: 400 });
@@ -46,17 +46,17 @@ export async function POST(
       return new NextResponse("Free trial has expired. Please upgrade to pro.", { status: 403 });
     }
 
-    const response = await openai.createImage({
-      prompt,
-      n: parseInt(amount, 10),
-      size: resolution,
-    });
+    // const response = await openai.createImage({
+    //   prompt,
+    //   n: parseInt(amount, 10),
+    //   size: resolution,
+    // });
 
     if (!isPro) {
       await incrementApiLimit();
     }
 
-    return NextResponse.json(response.data.data);
+    // return NextResponse.json(response.data.data);
   } catch (error) {
     console.log('[IMAGE_ERROR]', error);
     return new NextResponse("Internal Error", { status: 500 });
